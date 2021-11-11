5c205e6d741e869788213fe8c2203d3c44e9e0e4
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-10 21:56:51 -0300

    Add files via upload

---
+++ Desafioequacao2grau.py
@@ -0,0 +1,18 @@
+#DESAFIO 
+'CRIAR UM PROGRAMA QUE RESOLVE UMA EQUAÇÃO DO 2º GRAU...'
+from math import sqrt
+a= int(input("Qual o valor de a?")) #A variavel a necessita ser !=0 
+b= int(input("Qual o valor de b?"))
+c= int(input("Qual o valor de c?"))
+
+#utilizando bhaskara
+ #descobrindo o delta
+delta= ((b**2) - (4 * a * c))
+print("O delta é:",delta)
+
+#substituindo na formula de bhaskara
+x1= (-b + sqrt(delta)) / (2 * a)
+print("O valor de x1 é:", x1)
+
+x2= (-b - sqrt(delta)) / (2 * a)
+print("O valor de x2 é:", x2)
