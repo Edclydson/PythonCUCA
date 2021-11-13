cc2476b4a0d1b052d6b756cefd9ff3f32f81f3f5
Edclydson <edclydson.sousa@gmail.com>
2021-11-13 14:22:03 -0300

    main

--- aprovareprovaaluno.py
+++ aprovareprovaaluno.py
@@ -1,14 +1,14 @@
-#DESAFIO 
+# DESAFIO
 
-#programa que pede as notas do aluno e diz se foi aprovado ou reprovado
+# programa que pede as notas do aluno e diz se foi aprovado ou reprovado
 
-nota1= float(input("digite a primeira nota: "))
-nota2= float(input("digite a segunda nota: "))
+nota1 = float(input("digite a primeira nota: "))
+nota2 = float(input("digite a segunda nota: "))
 
-media= (nota1 + nota2) / 2
-if media>= 7:
+media = (nota1 + nota2) / 2
+if media >= 7:
     # aprovado
     print("Aluno Aprovado!")
 else:
-    #reprovado
+    # reprovado
     print("Aluno Reprovado!")
