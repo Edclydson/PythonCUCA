4ad841fb4f072de23a2c2d1cdf4f5d1222ea025b
Edclydson <edclydson.sousa@gmail.com>
2021-11-16 16:50:57 -0300

    organizado

--- desafiofor.py
+++ desafiofor.py
@@ -1,36 +1,36 @@
-#DESAFIO
-#PROGRAMA QUE PEDE NOME, SEXO, IDADE, COR DOS OLHOS(PRETO, CASTANHO OU VERDE), COR DO CABELO(PRETO, LOIRO, BRANCO)
-#O PROGRAMA DEVERÁ CALCULAR E MOSTRAR O TOTAL DE HOMENS E O TOTAL DE MULHERES 
-#O PROGRAMA TAMBEM DEVERÁ MOSTRAR O TOTAL DE PESSOAS MENORES DE IDADE
-#O TOTAL DE PESSOAS DE OLHOS PRETOS
-#O TOTAL DE PESSOAS DOS CABELOS BRANCOS
+# DESAFIO
+# PROGRAMA QUE PEDE NOME, SEXO, IDADE, COR DOS OLHOS(PRETO, CASTANHO OU VERDE), COR DO CABELO(PRETO, LOIRO, BRANCO)
+# O PROGRAMA DEVERÁ CALCULAR E MOSTRAR O TOTAL DE HOMENS E O TOTAL DE MULHERES
+# O PROGRAMA TAMBEM DEVERÁ MOSTRAR O TOTAL DE PESSOAS MENORES DE IDADE
+# O TOTAL DE PESSOAS DE OLHOS PRETOS
+# O TOTAL DE PESSOAS DOS CABELOS BRANCOS
 p = int(input("Quantas pessoas serão cadastradas?"))
 totH = 0
 totM = 0
 totMI = 0
 totOP = 0
 totCB = 0
-for n in range(0,p):
-    nome=input("Qual o nome? ")
-    sexo=input("Qual o sexo? ")
-    #SEXO
-    if sexo =="masculino":
-        totH= totH + 1
+for n in range(0, p):
+    nome = input("Qual o nome? ")
+    sexo = input("Qual o sexo? ")
+    # SEXO
+    if sexo == "masculino":
+        totH = totH + 1
     elif sexo == "feminino":
         totM = totM + 1
-    idade=int(input("Qual a idade? "))
-    #MENOR DE IDADE
-    if idade<18:
+    idade = int(input("Qual a idade? "))
+    # MENOR DE IDADE
+    if idade < 18:
         totMI = totMI + 1
     olhos = input("Qual a cor dos olhos? Castanhos, Preto ou verde: ")
-    #COR DOS OLHOS
+    # COR DOS OLHOS
     if olhos == "pretos":
         totOP = totOP + 1
     cabelos = input("Qual a cor dos cabelos? Preto, Loiro ou Branco: ")
-    #COR DOS CABELOS
+    # COR DOS CABELOS
     if cabelos == "branco":
         totCB = totCB + 1
-        
+
 print("Total de Homens: ", totH)
 print("Total de Mulheres: ", totM)
 print("Total de Menores de idade: ", totMI)
