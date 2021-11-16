b737cf378b6a7d14c02e405c569e0fccab46a297
Edclydson <edclydson.sousa@gmail.com>
2021-11-16 17:52:42 -0300

    atividade

---
+++ porcentagem.py
@@ -0,0 +1,13 @@
+p = int(input("Quantas pessoas serão cadastradas?"))
+totET = 0
+preto = 0
+for n in range(0, p):
+    etnia = input("Qual a etnia? Preto, Branco ou Amarelo: ")
+    if(etnia == "preto" or etnia == "preta"):
+        totET = totET + 1
+        preto = preto + 1
+    elif(etnia == "branco" or etnia == "amarelo"):
+        totET = totET + 1
+perc = 100 * (preto/totET)
+print("Há", preto, "pessoas que se consideram Pretas! ")
+print(perc, "%", "das pessoas cadastradas eram pretas!")
