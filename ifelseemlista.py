e09aa5584139fbc16710590f8789418fe9304f4e
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-19 15:34:21 -0300

    Update ifelseemlista.py

--- ifelseemlista.py
+++ ifelseemlista.py
@@ -1,3 +1,3 @@
 frutas= ["maça","banana","uva","maça","uva","laranja"]
-lista1 = [x if x != "banana" else "abacate" for x in frutas]
+lista1 = [x if x != "banana" else "abacate" for x in frutas] #SE x FOR DIFERENTE DE banana ELE PASSA P/ O PROXIMO, SENÃO ELE INSERE abacate NO LUGAR
 print(lista1)
