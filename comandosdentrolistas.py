def46d9b289faab7682ad6b68c71e9d7cb11597c
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-19 15:32:41 -0300

    Update comandosdentrolistas.py

--- comandosdentrolistas.py
+++ comandosdentrolistas.py
@@ -1,3 +1,3 @@
 frutas= ["maça","banana","uva","maça","uva","laranja"]
-lista1 = [x for x in frutas if "m" in x]
+lista1 = [x for x in frutas if "m" in x] #SE ENCONTRAR FRUTAS DENTRO DA LISTA frutas COM A LETRA m, JOGA DENTRO DA lista1
 print(lista1)
