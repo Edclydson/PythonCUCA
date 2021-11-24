bd57a0262ed55a8200b7d0795dfb1511ad197939
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-24 10:31:22 -0300

    Add files via upload

---
+++ listadicionario.py
@@ -0,0 +1,7 @@
+#LISTA DE DICIONARIO
+nonoAno = [{"nome":"joao","idade":39},
+            {"nome":"maria","idade":19},
+            {"nome":"victoria","idade":16},
+            {"nome":"jose","idade":23},
+            {"nome":"ana","idade":20}]
+print(nonoAno)
