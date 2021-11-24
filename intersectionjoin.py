e4a402a2aac2db80f4f59eed59630403bdb5b366
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-24 09:57:17 -0300

    Add files via upload

---
+++ intersectionjoin.py
@@ -0,0 +1,11 @@
+#join
+set1={"a","b","c"} 
+set2={1,2,3}
+set3=set1.union(set2)
+print(set3)
+
+#instersection
+x = {"maça","banana","uva"}
+y = {"google","microsoft","maça"}
+x.intersection_update(y)
+print(x)
