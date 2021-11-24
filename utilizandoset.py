780000c05e1576996bcb3bce2b22239e4fbe8703
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-24 09:28:22 -0300

    Add files via upload

---
+++ utilizandoset.py
@@ -0,0 +1,11 @@
+set1= {"celular","tv","geladeira","cama","tv","fogão"}
+print(set1)
+print(len(set1))
+print(type(set1))
+
+set2 = {1,2,3,4}
+set3= set((True,False,True))
+set4 = {"abc",34,True,40,"male"}
+print(set2)
+print(set3)
+print(set4)
