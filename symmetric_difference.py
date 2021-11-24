3b9dc5ff9bb8b3c31c89367c3b0a48b1c0d6200b
Edclydson Sousa <92530317+Edclydson@users.noreply.github.com>
2021-11-24 10:09:03 -0300

    Add files via upload

---
+++ symmetric_difference.py
@@ -0,0 +1,6 @@
+x = {"maça","banana","cherry"}
+y = {"google","microsoft","maça"}
+#x.symmetric_difference_update(y) #adicionando a x os itens diferentes entre x e y
+z = x.symmetric_difference(y) #adicionando a z os itens diferentes entre x e y
+print(x)
+print(z)
